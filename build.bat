410af06208bc1a4d54ca50d4ba268c94a01c67cd
howardthesheep <pwjones1997@gmail.com>
2020-08-10 02:38:14 -0500

    fixing up windows build script

--- build.bat
+++ build.bat
@@ -1,18 +1,25 @@
 @echo off
-echo "Setting Directory Paths..."
+echo Setting Directory Paths...
 set hamlDir=html
 set sassDir=css
 set outputDir=build
+set imgDir=img
 
-echo "Creating Build Directory.."
+echo.
+echo Creating Build Directory..
 if exist %outputDir% rmdir /S /Q %outputDir% 
 if not exist %outputDir% mkdir %outputDir% 
 
-echo "Compiling HAML"
+echo.
+echo Moving images into Build Directory...
+robocopy %imgDir% %outputDir%\%imgDir% /E /NJH /NJS /NFL /NDL
+
+echo.
+echo Compiling HAML
 if not exist %outputDir%\html mkdir %outputDir%\html
 for /f %%f in ('dir /b %hamlDir%') do call haml --trace %hamlDir%\%%f %outputDir%\html\%%~nf.html
 move %outputDir%\%hamlDir%\index.html %outputDir%\
 
-
-echo "Compiling Sass"
+echo.
+echo Compiling Sass
 call sass --style=compressed %sassDir%:%outputDir%\css
